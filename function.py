321fcb51f5b65e0121310786be70c372b1e1ef30
chandra447 <39896565+chandra447@users.noreply.github.com>
2023-04-28 19:26:13 +1000

    Final Changes to submit

--- function.py
+++ function.py
@@ -36,7 +36,7 @@ def file_exists(bucketName,path):
 
 def ingest(x,y):
     
-    bucket_name = 'ingest-bucket-foundary'
+    bucket_name = 'apiKeyIngest-foundary-prd'
     today = dt.date.today()
     year = today.year
     month = today.month
